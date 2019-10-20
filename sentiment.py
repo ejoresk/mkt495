cbfd516c89b757dbf118fb5591040d39f7cff9e0
Eliot Oreskovic <eliotoreskovic@gmail.com>
2019-10-20 03:59:17 -0400

    added comments

--- sentiment.py
+++ sentiment.py
@@ -2,12 +2,14 @@ from os import chdir, getcwd, listdir, mkdir, rename
 from os.path import join
 from textblob import TextBlob
 
+# navigate to directory w/ txt files
 directory = input("Directory name?")
 chdir(join(getcwd(), directory))
 ls = listdir()
 write_fl_name = directory + ".sentiment"
 write_fl = open(write_fl_name, "w")
 
+# iteratively sentiment-analyze txt files
 for i in ls:
 	read_fl = open(i, "r").read()
 	read_fl = TextBlob(read_fl)
@@ -17,6 +19,7 @@ for i in ls:
 
 write_fl.close()
 
+# navigate down a directory + move list of sentiment arrays to own directory
 chdir("..")
 mkdir("sentiments")
 rename(join(directory, write_fl_name), join("sentiments", write_fl_name))
